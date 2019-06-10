8830d74c1be8f20e0af08d534f976b31393df8fb
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-10 21:14:44 +0200

    Add params in js

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ for (var i = 0; moveButtons.length > i; i++) {
   moves.setAttribute('data-move',i+1); //add data-move 
   moves.addEventListener('click', function () { 
     var moveValue = this.getAttribute('data-move'); 
-    params.playerMove(moveValue); 
+    playerMove(moveValue); 
   })}
 buttonNewGame.addEventListener('click', function(){ 
   newGame(); 
@@ -47,27 +47,38 @@ function computerChoice() {
 function playerMove(playerM) {
   var computerMove=computerChoice();
   var moveNames=['Rock','Paper','Scissors'];
-  
+
   if (params.computerMove == params.playerM) { 
     // tie
     output.innerHTML = 'It`s a tie!';
+    params.progress.push({content: 'It`s a tie!'});
+            updateResoults();
   }
   else if ((params.computerMove == 2 && params.playerM == 1) || (params.computerMove == 3 && params.playerM == 2) || (params.computerMove == 1 && params.playerM == 3)) {
     // player lose
     output.innerHTML = 'Sorry, You lose! Computer played '+ moveNames[params.computerMove-1];
     updateResoults(0);
+    params.progress.push({content: 'Sorry, You lose! You palyed ' + moveNames[params.playerM-1] + 'Computer played '+ moveNames[params.computerMove-1]});
+            updateResoults();
   } 
   else {
     // player won
     output.innerHTML = 'Congrats, You won! Computer played '+ moveNames[params.computerMove-1];
     updateResoults(1);
+    params.progress.push({content: 'Congrats, You won! You palyed ' + moveNames[params.playerM-1] + 'Computer played '+ moveNames[params.computerMove-1]});
+            updateResoults();
   }   
-};
+}; 
 
 // game result and alert
 function updateResoults(playerwin) {
   var resultP=parseInt(resultPlayer.innerHTML);
   var resultC=parseInt(resultComputer.innerHTML);
+  var scoreTable = document.createElement('p');       
+    for (var c in params.progress) {
+        scoreTable.innerHTML = params.progress.length + '. ' + params.progress[c].content;
+        alert.appendChild(scoreTable);
+    } 
   if (playerwin) {
     resultP++;
     resultPlayer.innerHTML=params.resultP;
